117de6cd88863dafb0847fd831a68282d847d91c
Asaftei Rares <rares-stefan.asaftei@student.tuiasi.ro>
2024-12-08 19:29:45 +0200

    interfata Organ.py

--- code/Organ.py
+++ code/Organ.py
@@ -1,6 +1,8 @@
 from interface import Interface, implements
 
 class Organ(Interface):
+    def __init__(self):
+        pass
     def prepare_train(self, in_dir, pixdim=(1.0, 1.0, 1.0), spatial_size=[128, 128, 64], cache=True):
         pass
 
